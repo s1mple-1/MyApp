8ad0effb781eda48862afda99ef1cc7a70035e91
r.yurmanov@gmail.com <r.yurmanov@gmail.com>
2020-11-09 18:11:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/s1mple/myapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.s1mple.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
