34e12148f2be04b6fbfa36cd6cb37d6b3228da3b
r.yurmanov@gmail.com <r.yurmanov@gmail.com>
2020-11-28 13:30:29 +0300

    Приложение разбито на фрагменты Добавлены ресурсы

--- app/src/main/java/ru/s1mple/myapp/MainActivity.kt
+++ app/src/main/java/ru/s1mple/myapp/MainActivity.kt
@@ -1,21 +1,18 @@
 package ru.s1mple.myapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val view : ImageButton = findViewById(R.id.first_film)
-        view.setOnClickListener { nextScreen() }
-    }
-
-    private fun nextScreen() {
-        val intent = Intent(this, MovieDetailsActivity::class.java)
-        startActivity(intent)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainFrame, FragmentMoviesList())
+                .commit()
+        }
     }
 }
