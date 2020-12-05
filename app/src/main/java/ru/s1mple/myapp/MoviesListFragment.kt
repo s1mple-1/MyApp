770c5b3ffa9be570425b357e2c67d51561f9338a
Sergio <webanimal.group@gmail.com>
2020-12-05 22:15:10 +0300

    HomeWork Lists. Refactoring.

--- app/src/main/java/ru/s1mple/myapp/MoviesListFragment.kt
+++ app/src/main/java/ru/s1mple/myapp/MoviesListFragment.kt
@@ -1,49 +1,72 @@
 package ru.s1mple.myapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MoviesListFragment : Fragment() {
 
-    private var filmsListClickListener : FilmsListClickListener? = null
+    private var filmsClickListener : FilmClickListener? = null
     private var recycler: RecyclerView? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is FilmClickListener) {
+            filmsClickListener = context
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         super.onCreateView(inflater, container, savedInstanceState)
+
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-            recycler = view.findViewById(R.id.films_recycler)
-            recycler?.adapter = FilmsListAdapter(filmsListClickListener)
+
+        recycler = view.findViewById(R.id.films_recycler)
+        recycler?.apply {
+            layoutManager = GridLayoutManager(context, RECYCLER_COLUMN_COUNTER)
+            adapter = FilmsListAdapter(filmsClickListener)
+        }
     }
 
     override fun onStart() {
         super.onStart()
+
         updateData()
     }
 
     override fun onDetach() {
-        recycler = null
         super.onDetach()
+
+        filmsClickListener = null
+        recycler = null
     }
 
     private fun updateData() {
-        (recycler?.adapter as? FilmsListAdapter)?.apply {
-            bindFilms(FilmsDataSource().getFilms())
-        }
+        (recycler?.adapter as? FilmsListAdapter)?.bindFilms(FilmsDataSource().getFilms())
     }
 
-    fun setListener(l : FilmsListClickListener) {
-        filmsListClickListener = l
+    interface FilmClickListener {
+        fun onClick(filmId: Int)
+    }
+
+    companion object {
+        fun newInstance() = MoviesListFragment()
     }
 }
+
+private const val RECYCLER_COLUMN_COUNTER = 2
